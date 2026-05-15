d537d1abbcdf2134237a0b0b24836720747dace9
William Murillo <williammurillor15@gmail.com>
2026-05-15 14:20:39 -0500

    conectar angular con la url del backend

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // IMPORTANTE: Cambia esto por el dominio real de tu backend en Railway
-  apiUrl: 'giving-joy-production.up.railway.app' 
+  apiUrl: 'https://giving-joy-production.up.railway.app' 
 };
