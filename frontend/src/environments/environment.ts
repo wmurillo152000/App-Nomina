d211e8c03cdde822b6a2379345de7055e645bd27
William Murillo <williammurillor15@gmail.com>
2026-05-15 14:11:28 -0500

    conectar angular con la url del backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080' // Tu backend local de Java
+  apiUrl: 'giving-joy-production.up.railway.app' // Tu backend en railway de Java
 };
